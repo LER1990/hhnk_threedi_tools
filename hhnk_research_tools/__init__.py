2d64e79ad57199b98a7d8f5edca798fa501c23db
wvangerwen <41104904+wvangerwen@users.noreply.github.com>
2021-05-26 15:47:19 +0200

    add version to init

---
+++ hhnk_research_tools/__init__.py
@@ -0,0 +1,9 @@
+from hhnk_research_tools.variables import default_variables, definitions, types
+
+
+
+# TODO how does this work?
+# Threedigrid version number is automatic updated with zest.releaser
+# the version number in setup.py is updated using the find_version()
+
+__version__ = '0.1.0'
