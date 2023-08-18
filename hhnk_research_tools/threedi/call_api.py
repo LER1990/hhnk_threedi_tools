c8d6932319739f438f410a1b0c26075899a7bfab
Wietse van Gerwen <41104904+wvangerwen@users.noreply.github.com>
2023-08-18 08:54:20 +0200

    remove comments

--- hhnk_research_tools/threedi/call_api.py
+++ hhnk_research_tools/threedi/call_api.py
@@ -3,28 +3,26 @@ from threedi_api_client.openapi import ApiException
 import time
 
 
-def _call_threedi_api(func, max_retries_creation=60, **kwargs):
+def _call_threedi_api(func, max_retries=60, **kwargs):
     """add something to simulation, if apiexcetion is raised sleep on it and try again."""
     
-    for i in range(max_retries_creation):
-        
-        # while True:
-            try:
-                #check if data is dict
-                if "data" in kwargs.keys():
-                    if type(kwargs["data"]) != dict:
-                        print("OJEE... data is geen dict {func}")
-                        break
+    for i in range(max_retries):
+        try:
+            #check if data is dict
+            if "data" in kwargs.keys():
+                if type(kwargs["data"]) != dict:
+                    print("OJEE... data is geen dict {func}")
+                    break
 
-                r=func(**kwargs)
-                return r
-            except ApiException as e:
-                error=e
-                print(e)
-                if error.status==400:
-                    print(f"ERROR in {func}")
-                    print(error.body)
-                    raise e
-                else: #TODO add error code of API overload
-                    time.sleep(10)
-                    continue
+            r=func(**kwargs)
+            return r
+        except ApiException as e:
+            error=e
+            print(e)
+            if error.status==400:
+                print(f"ERROR in {func}")
+                print(error.body)
+                raise e
+            else: #TODO add error code of API overload
+                time.sleep(10)
+                continue
