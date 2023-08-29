711e7600ea78ff802e33352fc46d446a0e94d8d7
Daniel <daniel@d2hydro.nl>
2023-08-29 09:53:00 +0200

    Update file_class.py

--- hhnk_research_tools/folder_file_classes/file_class.py
+++ hhnk_research_tools/folder_file_classes/file_class.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-
 import json
 from hhnk_research_tools.general_functions import (
     get_functions,
@@ -13,46 +12,12 @@ class BasePath:
         self._base = base
         self.path = Path(str(base)).absolute().resolve()
 
-def get_functions(cls):
-    funcs = '.'+' .'.join([i for i in dir(cls) if not i.startswith('__') 
-                            and hasattr(inspect.getattr_static(cls,i)
-                            , '__call__')])
-    return funcs
-def get_variables(cls):
-    variables = '.'+' .'.join([i for i in dir(cls) if not i.startswith('__') 
-                            and not hasattr(inspect.getattr_static(cls,i)
-                            , '__call__')])
-    return variables
-
-
-# class File(type(Path()), Path):
-class File():
-    def __init__(self, base):
-        self.path = Path(str(base))
     # decorated properties
     @property
     def base(self): #path as posix string (foreward slashes)
         return self.path.as_posix()
 
-    #Path properties
     @property
-    def base(self): #full path to file
-        return self.path.as_posix()
-    @property
-    def name(self): #name with suffix
-        return self.path.name
-    @property
-    def stem(self): #stem (without suffix)
-        return self.path.stem
-    @property
-    def suffix(self):
-        return self.path.suffix
-    def unlink(self, missing_ok=True):
-        self.path.unlink(missing_ok=missing_ok)
-    @property
-    def parent(self):
-        return self.path.parent
-
     def name(self): #name with suffix
         return self.path.name
 
@@ -66,28 +31,6 @@ class File():
         import warnings
         warnings.warn(".pl is deprecated and will be removed in a future release. Please use .path instead", DeprecationWarning, stacklevel=2)
         return self.path
-
-    def read_json(self):
-        if self.path.suffix==".json":
-            return json.loads(self.path.read_text())
-        else:
-            raise Exception(f"{self.name} is not a json.")
-
-
-    def exists(self):
-        """dont return true on empty path."""
-        if self.base == ".":
-            return False
-        else:
-            return self.path.exists()
-        
-    @property
-    def path_if_exists(self):
-        """return filepath if the file exists otherwise return None"""
-        if self.exists():
-            return str(self.path)
-        else:
-            return None
 
     @property
     def path_if_exists(self):
