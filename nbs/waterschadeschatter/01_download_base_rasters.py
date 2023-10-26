9c1c55ea3bfb0333718dbe3023845c69be358fed
Wietse van Gerwen <41104904+wvangerwen@users.noreply.github.com>
2023-10-26 12:28:32 +0200

    raster download lizard v4

--- nbs/waterschadeschatter/01_download_base_rasters.py
+++ nbs/waterschadeschatter/01_download_base_rasters.py
@@ -1,18 +1,22 @@
+# %%
 """
 Klaarzetten van de landgebruiksrasters.
-
 """
+# import threedi_scenario_downloader.downloader as dl
+import hhnk_threedi_tools.external.downloader as dl
 
-
-# %%
-import threedi_scenario_downloader.downloader as dl
+import importlib
+importlib.reload(dl)
+import logging
+dl.set_logging_level(logging.DEBUG)
 import os
 from osgeo import gdal
 import numpy as np
 # from functions.create_folders_dict import create_folders_dict_wss
 # import functions.wsa_tools as wsa #general tools used across all scripts
 import hhnk_research_tools as hrt
-# %%
+from pathlib import Path
+
 # this allows GDAL to throw Python Exceptions
 gdal.UseExceptions()
 
@@ -25,11 +29,12 @@ dl.set_api_key(api_key=api_keys["lizard"])
 #uuid of raster
 uuids={}
 # uuids['landgebruik'] = 'b73189fc-058d-4351-9b20-2538248fae4f'
-uuids['landgebruik2019_3di'] = 'a80a6d31-f539-4037-9765-4fb880654424' #Deze wordt gebruikt in de 3Di nabewerking schadeberekening. Dat is hier terug te vinden; https://api.3di.live/v3.0/simulations/113368/results/post_processing/lizard/overview/
+# uuids['landgebruik2019_3di'] = 'a80a6d31-f539-4037-9765-4fb880654424' #Deze wordt gebruikt in de 3Di nabewerking schadeberekening. Dat is hier terug te vinden; https://api.3di.live/v3.0/simulations/113368/results/post_processing/lizard/overview/
 # uuids['landuse2018'] = '7a93a6de-680d-41ca-8e7a-4b356baa18c7'
 # uuids['landuse2019'] = '169853de-8319-4a7f-b076-f2ee0ae5b8c4'
 # uuids['landuse2020'] = 'afedc34b-427e-4fd1-ac19-ba83032e2407'
 # uuids['landuse2021'] = '59064cc0-a0a2-4509-b790-e112cb718100'
+uuids['hittestress'] = 'aac48ef4-247d-4dca-a878-35b2b6a8b460'
 
 
 # uuids['ahn3'] = '36588275-f3e3-4120-8c1e-602f7ae85386' #Deze is niet meer te vinden maar wel gebruikt in de schadeberekening van 2020. 
@@ -40,6 +45,7 @@ output_dir['landgebruik2019_3di'] = r'E:\01.basisgegevens\hhnk_schadeschatter\01
 output_dir['landuse2019'] = r'E:\01.basisgegevens\hhnk_schadeschatter\01_data\landuse2019_tiles'
 output_dir['landuse2020'] = r'E:\01.basisgegevens\hhnk_schadeschatter\01_data\landuse2020_tiles'
 output_dir['landuse2021'] = r'E:\01.basisgegevens\hhnk_schadeschatter\01_data\landuse2021_tiles'
+output_dir['hittestress'] = r'E:\01.basisgegevens\rasters\lizard_hittestress'
 
 #resolution of output raster.
 RESOLUTION = 0.5 #m
@@ -49,7 +55,7 @@ CHUNKSIZE = 5000
 outDirs = {}
 scenario_uuid=[]
 resolution=RESOLUTION
-bounds=[]
+bbox=[]
 pathname=[]
 raster_code=[]
 is_threedi_scenario=[]
@@ -60,7 +66,6 @@ bounds_hhnk = {'west': 100500.0,
   'south': 486900.0,
   'east': 150150.0,
   'north': 577550.0}
-
 
 
 xres = (bounds_hhnk['east']-bounds_hhnk['west'])/RESOLUTION
@@ -77,10 +82,12 @@ for key, uuid in uuids.items():
                 'south':bounds_hhnk['south']+ j*CHUNKSIZE*RESOLUTION,
                 'east':min(bounds_hhnk['west'] + (i+1)*CHUNKSIZE*RESOLUTION, bounds_hhnk['east']),
                 'north':min(bounds_hhnk['south']+ (j+1)*CHUNKSIZE*RESOLUTION, bounds_hhnk['north'])}
+            
+            bbox_v4 = f"{b['west']},{b['south']},{b['east']},{b['north']}"
             output_file = os.path.join(output_dir[key], f'{key}_x{str(i).zfill(2)}_y{str(j).zfill(2)}.tif')
             #Opsplitsen in kleinere gebieden
             if not os.path.exists(output_file):
-                bounds.append(b)
+                bbox.append(bbox_v4)
                 scenario_uuid.append(uuid)
                 pathname.append(output_file)
                 raster_code.append('')
@@ -91,7 +98,8 @@ for key, uuid in uuids.items():
         os.mkdir(output_dir[key])
     # #Create task and download rasters
     if len(pathname) != 0:
-        cont = input(f'Start downloading {len(pathname)} tiles. \nContinue? [y/n]')
+        cont='y'
+        # cont = input(f'Start downloading {len(pathname)} tiles. \nContinue? [y/n]')
 
 
 
@@ -104,16 +112,16 @@ for block in blocks:
     for key, uuid in uuids.items(): 
         if cont=='y':
             print('Start download')   
-
-            dl.download_raster(scenario=scenario_uuid[block],
-                            raster_code = raster_code[block],
-                            target_srs  = "EPSG:28992",
-                            resolution  = resolution,
-                            bounds      = bounds[block],
-                            bounds_srs  = "EPSG:28992",
-                            pathname    = pathname[block],
-                            is_threedi_scenario = is_threedi_scenario[block])
-                            
+            dl.download_raster(
+                        scenario=scenario_uuid[block],
+                        raster_code= raster_code[block],
+                        projection="EPSG:28992",
+                        resolution=resolution,
+                        bbox=bbox[block],
+                        pathname=pathname[block],
+                        is_threedi_scenario = is_threedi_scenario[block],
+                        export_task_csv=Path(pathname[block][0]).with_suffix(".txt")
+                    )      
 # %% Create VRT
 for key, uuid in uuids.items(): 
     hrt.build_vrt(output_dir[key], bandlist=None, overwrite=True)
