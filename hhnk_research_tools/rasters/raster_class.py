d83709875a54320665e123e9d0996234a4f1475d
wvangerwen <41104904+wvangerwen@users.noreply.github.com>
2024-06-23 12:54:49 +0200

    add options

--- hhnk_research_tools/rasters/raster_class.py
+++ hhnk_research_tools/rasters/raster_class.py
@@ -1,8 +1,11 @@
 import rioxarray as rxr
 from osgeo import gdal
 from hhnk_research_tools.folder_file_classes.file_class import File
+import rasterio as rio
+
+CHUNKSIZE=4096
 class Raster(File):
-    def __init__(self, base, chunksize=4096):
+    def __init__(self, base, chunksize=CHUNKSIZE):
         super().__init__(base)
 
         self.chunksize = chunksize
@@ -64,17 +67,44 @@ class Raster(File):
         return self._rxr
 
 
-    def open_gdal_source_read(self):
+    def open_gdal_read(self):
         """usage;
         with self.open_gdal_source_read() as gdal_src: doesnt work.
         just dont write it to the class, and it should be fine..
         """
         return gdal.Open(self.base, gdal.GA_ReadOnly)
 
-    def open_gdal_source_write(self):
+    def open_gdal_write(self):
         """Open source with write access"""
         return gdal.Open(self.base, gdal.GA_Update)
+
+    def open_rio_write(self):
+        return rio.open(self.base, 'r+')
     
+    def overviews_build(self, factors:list=[16], resampling="average"):
+        """Build overviews for faster rendering.
+        documentation: https://gdal.org/programs/gdaladdo.html
+        """
+        with self.open_rio_write() as dst:
+            dst.build_overviews(factors, getattr(rio.enums.Resampling, resampling))
+            dst.update_tags(ns='rio_overview', resampling=resampling)
+
+        #TODO this seems to give a nice result, can we incorparte it here somewhre or can rio do the same?
+        # !gdaladdo -ro -r nearest --config COMPRESS_OVERVIEW ZSTD --config PREDICTOR_OVERVIEW 2 --config ZSTD_LEVEL_OVERVIEW 1 "C:\Users\Wietse\Documents\HHNK\playground\dem_schemer_hoog_zuid_compressed_v4.tif" 8 32
+
+    
+    def overviews_available(self):
+        """Display available overviews on raster."""
+        src = rio.open(self.base, "r")
+        print([src.overviews(i) for i in src.indexes])
+
+    def overviews_remove(self):
+        """Remove overviews from raster"""
+        ds = self.open_gdal_source_write()
+        ds.BuildOverviews("NONE")
+        ds = None
+
+        print(f"Removed overviews: {self.view_name_with_parents(2)}")
 
     def statistics():
     def metadata():
@@ -89,6 +119,35 @@ class Raster(File):
         #Is er reden om dit niet te doen?
     
 
+
+    @classmethod
+    def write(cls, raster_out, result, dtype="float32", scale_factor=None, chunksize=CHUNKSIZE):
+        raster_out = cls(raster_out, chunksize=chunksize)
+
+        if "float" in dtype:
+            compress="LERC_DEFLATE"
+        else:
+            compress="ZSTD"
+
+        result.rio.to_raster(
+            raster_out.base,
+            # chunks={"x": CHUNKSIZE, "y": CHUNKSIZE},
+            chunks=True,
+            # lock=threading.Lock(), #Use dask multithread
+            lock=True,
+            tiled=True,
+            windowed=True, #If lock is False, window doesnt work.
+            COMPRESS=compress, #gdal options
+            PREDICTOR=2,#gdal options
+            ZSTD_LEVEL=1,#gdal options
+            NUM_THREADS="ALL_CPUS",#gdal options
+            dtype=dtype,
+        )
+
+        # Settings the scale_factor does not work with rioxarray. Update the result
+        if scale_factor:
+            with raster_out.open_rio_write() as src:
+                src.scales = (scale_factor,)
 
 
     #Bewerkingen
