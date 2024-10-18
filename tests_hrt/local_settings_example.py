07b346d9e490d27c873adbf3f65e898683e7e340
wvangerwen <41104904+wvangerwen@users.noreply.github.com>
2024-10-18 19:33:47 +0200

    Update local_settings_example.py

--- tests_hrt/local_settings_example.py
+++ tests_hrt/local_settings_example.py
@@ -1,7 +1,7 @@
 # Copy to local_settings.py and fill dict
 DATABASES = {
     "aquaprd_lezen": {
-        "service_name": "AQUAPRD",
+        "service_name": "",
         "user": "",
         "password": "",
         "host": "",  # empty string for localhost.
