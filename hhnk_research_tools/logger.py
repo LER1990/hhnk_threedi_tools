8c75f3ae79821c1ccc2176979eb9a32e831ca915
wvangerwen <41104904+wvangerwen@users.noreply.github.com>
2024-12-13 11:41:32 +0100

    do not log warnings to stderr

--- hhnk_research_tools/logger.py
+++ hhnk_research_tools/logger.py
@@ -39,14 +39,14 @@ def get_logconfig_dict(level_root="WARNING", level_dict=None, log_filepath=None)
             "debug_console_handler": {
                 "level": "NOTSET",
                 "formatter": "time_level_name",
-                "class": logging.StreamHandler,
-                "stream": sys.stdout,
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
             },
             "stderr": {
                 "level": "ERROR",
                 "formatter": "time_level_name",
-                "class": logging.StreamHandler,
-                "stream": sys.stderr,
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stderr",
             },
         },
         "formatters": {
